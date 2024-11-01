3545955156273d034ad0fc32914e9f2ff03018a0
onosb <onoriodeerekaye@gmail.com>
2024-11-01 17:04:08 +0100

    Create PlayRound and Playgame function
     Created playRound function that uses two parameters- humanChoice and computerChoice to get humanSelection and computerSelection. And then, call this function 5 times using a for loop to play 5 rounds. Next, created playGame function and move playRound function into it, and then created human score and computer score set to zero to keep track of the scores. Finally, used an if statement to determine the winner by comparing computer score to human score

--- script.js
+++ script.js
@@ -25,3 +25,50 @@ function getComputerChoice() {
         throw new Error("Unexpected value: " + computerChoice);
     }
   }
+// playround function and playgame function
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock" && computerChoice === "paper") {
+      console.log(`I win this round! Paper covers rock.
+        Computer Score: ${humanScore} || humanScore: ${++computerScore}`);
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+      console.log(`You win this round! Paper covers rock.
+        Human Score: ${++humanScore} || computerScore: ${computerScore}`);
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+      console.log(`I Win  this round! Rock crushes Scissors 
+        Computer Score: ${humanScore}  || humanScore: ${++computerScore}`);
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+      console.log(`You win this round! Rock crushes Scissors 
+       Human Score: ${++humanScore} || computerScore: ${computerScore}`);
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+      console.log(`You Win  this round! Scissors cuts Paper.
+         Human Score: ${++humanScore} || computerScore: ${computerScore}`);
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+      console.log(`I win this round! Scissors cuts paper.
+        Computer Score: ${humanScore} || humanScore: ${++computerScore}`);
+    } else {
+      console.log("It's a tie!");
+    }
+  }
+  for (i = 1; i < 6; i++) {
+    console.log(`Round ${i}`);
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+  }
+  if (humanScore > computerScore) {
+    console.log(
+      `Congrats! You're the overall Winner with a total of ${humanScore} points.  || I lose!`
+    );
+  } else if (computerScore > humanScore) {
+    console.log(
+      `Congrats, to me! I am the overall Winner with a total of ${computerScore} points.  || You're a loser!`
+    );
+  } else {
+    `It is a tie match at the end! The points are the same! Human Score: ${humanScore} ||  Computer score: ${computerScore}`;
+  }
+}
+playGame();
